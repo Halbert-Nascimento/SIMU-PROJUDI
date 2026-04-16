d790a399693dcf51f69e5081207eec97b0af2081
halbert-nascimento <halbert.fac@gmail.com>
2026-04-16 10:24:45 -0300

    feat: implementar gerenciamento de grupos com criação e edição de grupos

--- ciclos/views.py
+++ ciclos/views.py
@@ -3,10 +3,11 @@ from __future__ import annotations
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.http import Http404
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 
-from .forms import CicloSimulacaoForm
-from .permissions import pode_criar_ciclo
+from .forms import CicloSimulacaoForm, GrupoTrabalhoForm
+from .models import CargoSimulacao, CicloSimulacao, GrupoTrabalho
+from .permissions import pode_criar_ciclo, pode_gerenciar_grupos_ciclo
 
 
 @login_required
@@ -32,6 +33,53 @@ def criar_ciclo(request):
 
 
 @login_required
-def gerenciar_grupos(request):
-    nome_ciclo = request.GET.get("ciclo", "")
-    return render(request, "ciclos/gerenciar_grupos.html", {"nome_ciclo": nome_ciclo})
+def gerenciar_grupos(request, ciclo_id):
+    ciclo = get_object_or_404(
+        CicloSimulacao.objects.select_related("status", "coordenador"),
+        pk=ciclo_id,
+    )
+
+    if not pode_gerenciar_grupos_ciclo(request.user, ciclo):
+        raise Http404()
+
+    grupos = ciclo.grupos.select_related("cargo_simulacao").order_by("nome")
+    cargos = CargoSimulacao.objects.all()
+
+    grupo_editando = None
+    form = GrupoTrabalhoForm(ciclo=ciclo)
+    reabrir_modal = False
+
+    if request.method == "POST":
+        grupo_id = request.POST.get("grupo_id") or None
+        if grupo_id:
+            grupo_editando = get_object_or_404(GrupoTrabalho, pk=grupo_id, ciclo=ciclo)
+            form = GrupoTrabalhoForm(request.POST, instance=grupo_editando, ciclo=ciclo)
+            acao = "atualizado"
+        else:
+            form = GrupoTrabalhoForm(request.POST, ciclo=ciclo)
+            acao = "criado"
+
+        if form.is_valid():
+            grupo = form.save(commit=False)
+            grupo.ciclo = ciclo
+            grupo.save()
+            messages.success(
+                request,
+                f'Grupo "{grupo.nome}" {acao} com sucesso.',
+                extra_tags="grupo",
+            )
+            return redirect("ciclos:gerenciar_grupos", ciclo_id=ciclo.pk)
+        else:
+            reabrir_modal = True
+            for erros in form.errors.values():
+                for erro in erros:
+                    messages.error(request, erro, extra_tags="grupo")
+
+    return render(request, "ciclos/gerenciar_grupos.html", {
+        "ciclo": ciclo,
+        "grupos": grupos,
+        "cargos": cargos,
+        "form": form,
+        "grupo_editando": grupo_editando,
+        "reabrir_modal": reabrir_modal,
+    })
