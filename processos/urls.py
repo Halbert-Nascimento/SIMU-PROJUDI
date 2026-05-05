22f830b75fef824fb03fa8eaa4f3e305abc30760
Eduardo Netto Freyer <51035176+EduardoFreyer@users.noreply.github.com>
2026-05-05 10:17:08 -0300

    Criando a tela do aluno

--- processos/urls.py
+++ processos/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from .views import cadastrar_processo, varas_por_comarca
+from .views import cadastrar_processo, pagina_aluno, varas_por_comarca
 
 app_name = "processos"
 
 urlpatterns = [
     path("cadastrar/", cadastrar_processo, name="cadastrar_processo"),
+    path("area-servidor/", pagina_aluno, name="pagina_aluno"),
     path(
         "api/varas/<int:comarca_id>/",
         varas_por_comarca,
